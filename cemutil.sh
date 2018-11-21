4470b93d1f7faabc7b897df416e7e8b27e5d1727
RealJohnGalt <johngalt@fake.mail>
2018-11-21 12:53:16 -0800

    Fix older wine compatibility

--- cemutil.sh
+++ cemutil.sh
@@ -144,7 +144,7 @@ if [ -z `winetricks list-installed|grep vcrun2015` ]; then
   winetricks settings win7
 fi
 cd $(realpath $instdir)
-mesa_glthread=true export __GL_THREADED_OPTIMIZATIONS=1 vblank_mode=0 WINEESYNC=0 wine $(realpath $instdir)/Cemu.exe "\$@"
+mesa_glthread=true export __GL_THREADED_OPTIMIZATIONS=1 vblank_mode=0 WINEESYNC=0 wine Cemu.exe "\$@"
 EOF1
 chmod +x LaunchCEMU
 
@@ -162,7 +162,7 @@ if [ -z `winetricks list-installed|grep vcrun2015` ]; then
   winetricks settings win7
 fi
 cd $(realpath $instdir)
-R600_DEBUG=nohyperz mesa_glthread=true vblank_mode=0 WINEESYNC=0 wine $(realpath $instdir)/Cemu.exe "\$@"
+R600_DEBUG=nohyperz mesa_glthread=true vblank_mode=0 WINEESYNC=0 wine Cemu.exe "\$@"
 EOF1
 chmod +x LaunchCEMUgcn3BOTW
 
