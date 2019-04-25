59192d06e8726614855e87ce9f1de2998447874f
HengiFettlich <hengifett@gmail.com>
2019-04-25 22:29:17 +0200

    added wine-staging installer for Ubuntu 18.04 and later

---
+++ ubuntu-wine.sh
@@ -0,0 +1,56 @@
+if [ -z "$DISPLAY" ]; then
+    export DISPLAY=:0.0
+fi
+
+sudo mkdir /tmp/wine_install
+
+cd /tmp/wine_install
+
+# add i386 architecture, necessary for wine
+sudo dpkg --add-architecture i386
+
+# download wine repo key
+sudo wget -nc https://dl.winehq.org/wine-builds/winehq.key
+
+# add wine repo key
+sudo apt-key add winehq.key
+
+# adds repo for Ubuntu 18.04 and libfaudio0, which isn't available in the standard repo
+if $(lsb_release -a | grep -q -e "18.04"); then
+    sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+    sudo wget -nc https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/amd64/libfaudio0_19.04-0~bionic_amd64.deb
+    sudo wget -nc https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/i386/libfaudio0_19.04-0~bionic_i386.deb
+fi
+
+# adds repo for Ubuntu 18.10 and libfaudio0, which isn't available in the standard repo
+if $(lsb_release -a | grep -q -e "18.10"); then
+    sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ cosmic main'
+    sudo wget -nc https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Ubuntu_18.10_standard/amd64/libfaudio0_19.04-0~cosmic_amd64.deb
+    sudo wget -nc https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Ubuntu_18.10_standard/i386/libfaudio0_19.04-0~cosmic_i386.deb
+fi
+
+# adds repo for Ubuntu 19.04 and libfaudio0, which isn't available in the standard repo
+if $(lsb_release -a | grep -q -e "19.04"); then
+    sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ cosmic main'
+    sudo wget -nc https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_19.04/amd64/libfaudio0_19.04-0~disco_amd64.deb
+    sudo wget -nc https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_19.04/i386/libfaudio0_19.04-0~disco_i386.deb
+fi
+
+# install faudio
+sudo dpkg -i *.deb
+
+sudo apt --fix-broken install -qq
+
+sudo apt update -qq
+
+# install wine-staging
+sudo apt install --install-recommends winehq-staging -qq
+
+# install winetricks
+sudo apt install winetricks -qq
+
+# vcrun2017 is recommended
+winetricks vcrun2017
+
+# cleanup
+rm -rf /tmp/wine_install
