2775573f861ceb701d06bf915008645673da9275
HengiFettlich <43856260+HengiFettlich@users.noreply.github.com>
2019-08-31 11:47:24 +0200

    Update ubuntu-wine.sh

--- ubuntu-wine.sh
+++ ubuntu-wine.sh
@@ -2,6 +2,11 @@ if [ -z "$DISPLAY" ]; then
     export DISPLAY=:0.0
 fi
 
+if ! $(lsb_release -a | grep -q -e "18.04" -e "18.10" -e "19.04"); then
+  echo "You need at least Ubuntu 18.04"
+  exit 1
+fi
+
 # add i386 architecture, necessary for wine
 sudo dpkg --add-architecture i386
 
@@ -10,11 +15,6 @@ sudo wget -nc https://dl.winehq.org/wine-builds/winehq.key
 
 # add wine repo key
 sudo apt-key add winehq.key
-
-if ! $(lsb_release -a | grep -q -e "18.04" -e "18.10" -e "19.04"); then
-  echo "You need at least Ubuntu 18.04"
-  exit 1
-fi
 
 if $(lsb_release -a | grep -q -e "18.04"); then
   sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
